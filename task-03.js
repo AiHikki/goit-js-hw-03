f647545b8968952436e7b5b58775d7c2b8f8c52c
AiHikki <freeomareps@gmail.com>
2023-10-24 23:08:30 +0300

    update

---
+++ task-03.js
@@ -0,0 +1,15 @@
+'use strict';
+const filterArray = function (numbers, value) {
+  const nums = [];
+  for (const num of numbers) {
+    if (num > value) {
+      nums.push(num);
+    }
+  }
+  return nums;
+};
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
